47c2cd01b42c84175dac837787fcfa55cd08a7c1
Marting05 <martin.restrepo05@gmail.com>
2026-08-16 17:25:08 -0500

    nuevos cambios matrices objetuales

--- metodos.java
+++ metodos.java
@@ -13,6 +13,7 @@ public class metodos {
             for (int j = 0; j < m.length; j++) {
                 System.out.print(" - "+m[i][j]);
             }
+            System.out.println();
         }
     }
  
@@ -32,6 +33,30 @@ public class metodos {
             for (int j = 0; j < m.length; j++) {
                 System.out.print(" - "+m[i][j].getDato());
             }
+            System.out.println();
         }
-    }    
+    }  
+    
+    public void MostrarDiagionalSuperior(ObjMatriz [][] m){
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m.length; j++) {
+                if(i==j){
+                    System.out.print(" - "+m[i][j].getDato());
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+
+    public void MostrarPar(int[][] m){
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m.length; j++) {
+                if(m[i][j]%2 == 0){
+                    System.out.println(" - "+m[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
 }
